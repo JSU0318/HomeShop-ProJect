c6494e871e25c47133a315c51bc4cbd7080462e6
Sang Uk Jung <83222288+JSU0318@users.noreply.github.com>
2023-05-08 17:58:17 +0900

    test3

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,17 @@
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
+
+// const YellowBtn = styled.button`
+//   background: yellow;
+//   color: black;
+//   padding: 10px;
+// `;
+
+// const Box = styled.div`
+//   background: grey;
+//   padding: 20px;
+// `;
 function Detail(props) {
   let { id } = useParams();
   let findItem = props.shoes.find(function (x) {
