2c7c5c509f78fa7955b87d1a105c38fdc6078ed7
Sang Uk Jung <83222288+JSU0318@users.noreply.github.com>
2023-05-08 17:45:58 +0900

    test2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,81 @@
+import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Button } from "react-bootstrap";
+import { Container, Navbar, Nav } from "react-bootstrap";
+import data from "./data.js";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import Detail from "./routes/Detail";
+
 function App() {
+  let [shoes] = useState(data);
+  let navigate = useNavigate();
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>{" "}
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">ShoeSHOP</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              Home
+            </Nav.Link>
+
+            <Nav.Link
+              onClick={() => {
+                navigate("/detail");
+              }}
+            >
+              Detail
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="main-bg"></div>
+              <div className="container">
+                <div className="row">
+                  {shoes.map((a, i) => {
+                    return <Card shoes={shoes[i]} i={i} />;
+                  })}
+                </div>
+              </div>
+            </>
+          }
+        />
+        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
+        <Route path="*" element={<div>404 ForBidden</div>} />
+
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>멤버들</div>} />
+          <Route path="location" element={<div>회사위치임</div>} />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h4>회사정보</h4>
+      <Outlet></Outlet>
+    </div>
+  );
+}
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img src={"https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"} width="80%" />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
     </div>
   );
 }
